da6b159d9b11cd6775cbef4618f98b73f80dc1e4
Vinovrski Alexandre <vinovrski.alexande@ulb.be>
2023-09-15 13:52:26 +0200

    - Added Splashscreen

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:codex_woltiensis/song_list.dart';
+import 'package:codex_woltiensis/spash_screen.dart';
 
 class App extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-    return MaterialApp(home: SongList());
+    return MaterialApp(home: SplashScreen());
   }
 }
---
+++ lib/spash_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:codex_woltiensis/song_list.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    Future.delayed(const Duration(seconds: 2), (){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder:
+      (BuildContext context) => SongList()));
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: [SystemUiOverlay.top, SystemUiOverlay.bottom]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      decoration: BoxDecoration(
+        gradient: _buildLinearGradient(),
+        image: _buildDecorationImage(),
+      ),
+    ));
+  }
+
+  DecorationImage _buildDecorationImage() {
+    return const DecorationImage(
+          image:  AssetImage('assets/images/splash_screen.png'),
+      );
+  }
+
+  LinearGradient _buildLinearGradient() {
+    return const LinearGradient(
+      begin: Alignment.bottomLeft,
+      end: Alignment.topRight,
+      colors: <Color>[Color(0xFFEC2143), Color(0xFF12A113)],
+    );
+  }
+}
